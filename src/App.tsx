00bfbf934f4c832871e2a64998af378aaa28344a
ooleksander <denisovProg@gmail.com>
2022-08-18 07:49:51 +0300

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  function start() {
-    let wordsArray = [
-      { id: 1, question: "why", answer: "because" },
-      { id: 2, question: "what", answer: "takiedela" },
-    ];
+  let wordsArray = [
+    { id: 1, question: "why", answer: "because" },
+    { id: 2, question: "what", answer: "takiedela" },
+  ];
 
-    const [gameProgress, setGameProgress] = useState(0);
+  const [gameMistakes, setGameMistakes] = useState(0);
+  const [randomWord, setRandomWord] = useState("");
+  const [gameIsStarted, setGameIsStarted] = useState(false);
+  const [currentLetter, setCurrentLetter] = useState("")
 
-    let word = getRandomWord();
-
-    function getRandomArbitrary(min: number, max: number) {
-      return Math.round(Math.random() * (max - min) + min);
+  useEffect(() => {
+    if (gameIsStarted) {
+      setGameMistakes(() => gameMistakes + 1);
+      console.log(gameMistakes);
+    } else {
+      setGameMistakes(0);
+      setRandomWord("");
+      setGameIsStarted(false);
     }
+  }, [gameIsStarted]);
 
-    function getRandomWord() {
-      let randomId = getRandomArbitrary(1, wordsArray.length);
-      let word = wordsArray.filter((word) => {
-        // Кажись криво отфильтровал
-        return word.id === randomId;
-      });
-      return word[0];
-    }
-
-    function onSubmit(letter: string) {
-      if (letter.length > 1) {
-        console.log("многа букв");
-        return;
-      }
-      letter = letter.toLowerCase();
-      checkLetterInAnswer(letter);
-    }
-
-    function checkLetterInAnswer(letter) {}
+  function getRandomArbitrary(min: number, max: number) {
+    return Math.round(Math.random() * (max - min) + min);
   }
+
+  function getRandomWord() {
+    let randomId = getRandomArbitrary(1, wordsArray.length);
+    let word = wordsArray.filter((word) => {
+      return word.id === randomId;
+    });
+    return word[0];
+  }
+
   return (
     <div>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          let letter = event.target.querySelector("input").value;
-          onSubmit(letter);
-        }}
-      >
-        <input type="text" />
-      </form>
-      <button></button>
+      <input maxLength={1} type="text" />
+      <button
+        onClick={() => setGameIsStarted((current: boolean) => !current)}
+      ></button>
     </div>
   );
 }
