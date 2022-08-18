b50846cb429ad3012e49da10163f53f06dcf7926
ooleksander <denisovProg@gmail.com>
2022-08-19 02:08:16 +0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import { useEffect, useState } from "react";
 
 function App() {
   let wordsArray = [
-    { id: 1, question: "why", answer: "because" },
+    { id: 1, question: "why", answer: "otvet" },
     { id: 2, question: "what", answer: "takiedela" },
   ];
 
   const [gameMistakes, setGameMistakes] = useState(0);
   const [randomWord, setRandomWord] = useState("");
   const [gameIsStarted, setGameIsStarted] = useState(false);
-  const [currentLetter, setCurrentLetter] = useState("")
+  const [currentLetter, setCurrentLetter] = useState("");
 
   useEffect(() => {
     if (gameIsStarted) {
-      setGameMistakes(() => gameMistakes + 1);
-      console.log(gameMistakes);
+      const gameElement = getRandomWord();
+      const gameWord = gameElement.answer
+      console.log(gameWord)
+      setRandomWord(gameWord);
+      console.log(randomWord);
     } else {
       setGameMistakes(0);
       setRandomWord("");
@@ -34,12 +37,28 @@ function App() {
     return word[0];
   }
 
+  function changeLetter(event: any) {
+    event.preventDefault();
+    console.log(event.target.letter.value);
+    setCurrentLetter(() => event.target.letter.value);
+    console.log(currentLetter);
+  }
+
   return (
     <div>
-      <input maxLength={1} type="text" />
-      <button
-        onClick={() => setGameIsStarted((current: boolean) => !current)}
-      ></button>
+      <form onSubmit={(event) => changeLetter(event)}>
+        <input
+          style={{ width: "100px" }}
+          name="letter"
+          maxLength={1}
+          type="text"
+         // onChange={}
+        />
+        <input type="submit" value={"Отправить"} />
+      </form>
+      <button onClick={() => setGameIsStarted((current: boolean) => !current)}>
+        Start
+      </button>
     </div>
   );
 }
